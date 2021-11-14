a022d31c96296cc8dbdbb19700b5be998f59c447
unknownadii <aditya.singh_cs19@gla.ac.in>
2021-11-14 13:35:28 +0530

    half dialog completed

--- app/src/main/java/com/example/spotfitness/MainActivity.kt
+++ app/src/main/java/com/example/spotfitness/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.spotfitness
 
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.Window
+import android.view.*
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,28 +20,27 @@ class MainActivity : AppCompatActivity() {
 
         iv_menuBar.setOnClickListener {
             showDialog()
-            Toast.makeText(this, "opening profile ", Toast.LENGTH_SHORT).show()
+
         }
         val homeFragment = HomeFragment()
         createFragment(homeFragment)
         //for the bottom navigation bar
         bottomNavigationView.background = null
         bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId)
-            {
-                R.id.home_icon ->{
+            when (it.itemId) {
+                R.id.home_icon -> {
                     Toast.makeText(this, "home selected", Toast.LENGTH_SHORT).show()
                 }
-                R.id.nutrition_icon ->{
+                R.id.nutrition_icon -> {
                     Toast.makeText(this, "nutrition_icon selected", Toast.LENGTH_SHORT).show()
                 }
-                R.id.exercises_icon ->{
+                R.id.exercises_icon -> {
                     Toast.makeText(this, "exercises_icon selected", Toast.LENGTH_SHORT).show()
                 }
-                R.id.history_icon ->{
+                R.id.history_icon -> {
                     Toast.makeText(this, "history_icon selected", Toast.LENGTH_SHORT).show()
                 }
-                R.id.bmi_icon ->{
+                R.id.bmi_icon -> {
                     Toast.makeText(this, "bmi_icon selected", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -53,13 +53,22 @@ class MainActivity : AppCompatActivity() {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(false)
         dialog.setContentView(R.layout.profile_dialog)
-        val testingButton=dialog.findViewById<Button>(R.id.testingButton)
+        val testingButton = dialog.findViewById<Button>(R.id.testingButton)
+        val profileClose = dialog.findViewById<ImageView>(R.id.profileClose)
 
-        testingButton.setOnClickListener {
-            dialog.cancel()
+        profileClose.setOnClickListener {
+            dialog.dismiss()
         }
         dialog.show()
 
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        //animation
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        dialog.window!!.getAttributes().windowAnimations = R.style.CustomDialogAnimation;
+        dialog.window!!.setGravity(Gravity.CENTER)
     }
 
     private fun createFragment(item: Fragment) {
