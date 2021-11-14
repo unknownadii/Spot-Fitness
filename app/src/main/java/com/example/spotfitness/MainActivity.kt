1d7ff94ed96d37ebf9b2612a4d9c1a4b7216fdf9
unknownadii <aditya.singh_cs19@gla.ac.in>
2021-11-14 12:18:33 +0530

    dialog corrected

--- app/src/main/java/com/example/spotfitness/MainActivity.kt
+++ app/src/main/java/com/example/spotfitness/MainActivity.kt
@@ -46,18 +46,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showDialog(title: String) {
-        val dialog = Dialog(activity)
+        val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(false)
-        dialog.setContentView(R.layout.custom_layout)
-        val body = dialog.findViewById(R.id.body) as TextView
-        body.text = title
-        val yesBtn = dialog.findViewById(R.id.yesBtn) as Button
-        val noBtn = dialog.findViewById(R.id.noBtn) as TextView
-        yesBtn.setOnClickListener {
-            dialog.dismiss()
-        }
-        noBtn.setOnClickListener { dialog.dismiss() }
+        dialog.setContentView(R.layout.profile_dialog)
+
         dialog.show()
 
     }
