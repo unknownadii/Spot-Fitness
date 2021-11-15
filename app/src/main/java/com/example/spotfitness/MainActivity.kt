7944e7078bba0c7dd9dd3c05e63b647794f7bae4
Mayank Verma <mayank.verma_cs19@gla.ac.in>
2021-11-15 13:29:31 +0530

    final profile completed

--- app/src/main/java/com/example/spotfitness/MainActivity.kt
+++ app/src/main/java/com/example/spotfitness/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
@@ -54,11 +55,29 @@ class MainActivity : AppCompatActivity() {
         dialog.setCancelable(false)
         dialog.setContentView(R.layout.profile_dialog)
         val profileClose = dialog.findViewById<ImageView>(R.id.profileClose)
+        val logout = dialog.findViewById<ImageView>(R.id.logout_btn)
+        val editBtnProfile = dialog.findViewById<Button>(R.id.edit_btnProfile)
+        val submitBtnProfile = dialog.findViewById<Button>(R.id.submit_btnProfile)
+        val lvSavedProfile = dialog.findViewById<LinearLayout>(R.id.lv_savedProfileData)
+        val lvEditProfile = dialog.findViewById<LinearLayout>(R.id.lv_editProfileData)
 
         profileClose.setOnClickListener {
             dialog.dismiss()
         }
 
+        editBtnProfile.setOnClickListener {
+            lvSavedProfile.visibility=View.GONE
+            lvEditProfile.visibility=View.VISIBLE
+        }
+        submitBtnProfile.setOnClickListener {
+            lvSavedProfile.visibility=View.VISIBLE
+            lvEditProfile.visibility=View.GONE
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+        }
+        logout.setOnClickListener {
+            Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
         dialog.window!!.setLayout(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
